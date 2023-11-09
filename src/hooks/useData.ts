81dd54ae39ce7dabb674a5bbd9043496ae0ca1f6
Abdul Halim Chowdhury <salman@inverseai.com>
2023-11-09 19:35:07 +0600

    Fetch Genres using React Query

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { AxiosRequestConfig, CanceledError } from "axios";
 
-interface FetchedResponse<T> {
+export interface FetchedResponse<T> {
   count: number;
   results: T[];
 }
