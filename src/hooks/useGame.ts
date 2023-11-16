03a20cbc915b9a55c9335fa3c512f4cf5a76f99f
Abdul Halim Chowdhury <salman@inverseai.com>
2023-11-16 18:53:49 +0600

    Fixed undefined error

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -6,7 +6,7 @@ const apiClient = new APIClient<Game>(`/games`);
 
 const useGame = (slug: string) => {
   return useQuery({
-    queryKey: ["genres"],
+    queryKey: ["games", slug],
     queryFn: () => apiClient.get(slug),
     // staleTime: 24 * 60 * 60 * 1000, // 24hrs
   });
